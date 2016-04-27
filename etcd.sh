c7ea9e7af64878ec1bb642daec7391b9c947edbb
Tomáš Kukrál <kukratom@fit.cvut.cz>
2016-04-27 12:50:45 +0200

    etcd bootstrap

--- etcd.sh
+++ etcd.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-# create data_etcd
-docker ps -a | grep data_etcd || docker create -v /var/etcd/data --name data_etcd tomkukral/gentoo-etcd /bin/true
+# https://coreos.com/etcd/docs/latest/clustering.html#static
+# https://coreos.com/etcd/docs/latest/docker_guide.html
 
+ETCD_IMAGE="quay.io/coreos/etcd:v2.2.0"
+ETCD_NAME="0 "
+ETCD_LISTEN_IP="0.0.0.0"
+ETCD_ADVERTISE_IP="127.0.0.1"
+
+# create data container
+docker ps -a | grep data_etcd || docker create -v /var/etcd/data --name data_etcd $ETCD_IMAGE /bin/true
 
 # remove etcd
-docker rm etcd
+docker stop etcd && docker rm etcd
 
+# start etcd
 docker run \
 	--name etcd \
 	--detach \
 	--restart always \
-	--net host \
 	--volumes-from data_etcd \
-	tomkukral/gentoo-etcd etcd \
-		--name {{kube_nodes[inventory_hostname].id}} \
+	-p 2380:2380 \
+	-p 4001:4001 \
+	$ETCD_IMAGE \
+		--name $ETCD_NAME \
 		--data-dir /var/etcd/data \
-		--advertise-client-urls http://127.0.0.1:4001 \
-		--listen-client-urls http://127.0.0.1:4001 \
-		--listen-peer-urls http://{{ ansible_dbr0.ipv4.address }}:4000 \
-		--initial-advertise-peer-urls http://{{ ansible_dbr0.ipv4.address }}:4000 \
+		--advertise-client-urls http://${ETCD_ADVERTISE_IP}:4001 \
+		--listen-client-urls http://${ETCD_LISTEN_IP}:4001 \
+		--listen-peer-urls http://${ETCD_LISTEN_IP}:2380 \
+		--initial-advertise-peer-urls http://${ETCD_ADVERTISE_IP}:2380 \
 		--initial-cluster-token etcd-cluster-1 \
-		--initial-cluster 0=http://172.17.0.1:4000,1=http://172.17.1.1:4000,2=http://172.17.2.1:4000 \
-		--initial-cluster-state existing
+		--initial-cluster 0=http://${ETCD_ADVERTISE_IP}:2380 \
+		--initial-cluster-state new
